d64eb7bb75076e664be7154d282c16572168c4b7
AumDigital <dave.church721@gmail.com>
2023-03-22 09:54:13 +0100

    function

--- script.js
+++ script.js
@@ -97,68 +97,87 @@
 //         // default will be executed if all other options fail
 // // }
 
-const day = "monday";
+// const day = "monday";
 
-if (day === "monday") {
-    console.log("Plan my javacsript course!");
-    console.log("Got to the gym");
-} else if (day === "tuesday") {
-    console.log("prepare for theory lesson");
-} else if (day === "wednesday" || day === "thursday") {
-    console.log("write some code examples");
-} else if (day === "friday") {
-    console.log("Give students feedback");
-} else if (day === "saturday" || day === "sunday") {
-    console.log("Enjoy your weekend");
-} else {
-    console.log("Not a valid day");
+// if (day === "monday") {
+//     console.log("Plan my javacsript course!");
+//     console.log("Got to the gym");
+// } else if (day === "tuesday") {
+//     console.log("prepare for theory lesson");
+// } else if (day === "wednesday" || day === "thursday") {
+//     console.log("write some code examples");
+// } else if (day === "friday") {
+//     console.log("Give students feedback");
+// } else if (day === "saturday" || day === "sunday") {
+//     console.log("Enjoy your weekend");
+// } else {
+//     console.log("Not a valid day");
+// }
+
+// // THE CONDITIONAL OPERATOR, TERNARY
+
+// const age = 25;
+
+// age >= 18 
+// //condition
+// ? console.log(`I like to drink beer`) 
+// // if
+// : console.log("I'll drink water.."); 
+// //else
+
+//  let n = "1" + 1;
+//  n = n - 1;
+//  console.log(n);
+
+//  //ARRAYS
+
+//  // A data structure which acts as a big container where we can store stuff if we have more than one value. 
+
+//  const dogs = ["Tore", "Clementine", "Doris"];
+//  console.log(dogs);
+
+//  const years = [1985, 1990, 1994];
+ 
+//  console.log(dogs);
+
+//  dogs[
+// 0] = "Trosan";
+//  console.log(dogs);
+
+//  const dogName = "Tore";
+// const tore = [dogName, "Miniature Bull Terrier", 2021-2018, "He likes sleeping in his fluffy bed", dogs];
+
+// console.log(tore);
+// console.log(tore.length);
+// console.log(tore[2]);
+
+
+// const calcAge = function(birthYear) { // Function to calculate age
+// 	return 2021 - birthYear;
+// };
+
+// const yearOfBirth = [1999, 1985, 2001, 2016, 1978]; // Array with birth years
+
+// const firstAge = calcAge(yearOfBirth[0]); // You need to do each calculation seperately
+
+// const secondAge = calcAge(yearOfBirth[1]); // Seclecting which array index to calculate.
+
+// console.log(firstAge, secondAge);
+
+
+
+function chocolateCake(butter, chocolate, eggs){ // Parameters defined in curly braces.
+	const cake = `A simple cake with ${butter} cup of butter, ${chocolate} cups of chocolate and ${eggs} eggs`;  
+    return cake;
 }
 
-// THE CONDITIONAL OPERATOR, TERNARY
+// chocolateCake(1, 2, 3); // Gives each parameter a value
+const yummyCake = chocolateCake(1, 2, 3);  
+console.log(yummyCake);
 
-const age = 25;
+function calcAge(age1, age2) { // Naming function - allocate blank parameters
+	const resAge = age1 + age2;  // defines the result of the function
+  return resAge; 
+}
 
-age >= 18 
-//condition
-? console.log(`I like to drink beer`) 
-// if
-: console.log("I'll drink water.."); 
-//else
-
- let n = "1" + 1;
- n = n - 1;
- console.log(n);
-
- //ARRAYS
-
- // A data structure which acts as a big container where we can store stuff if we have more than one value. 
-
- const dogs = ["Tore", "Clementine", "Doris"];
- console.log(dogs);
-
- const years = [1985, 1990, 1994];
- 
- console.log(dogs);
-
- dogs[0] = "Trosan";
- console.log(dogs);
-
- const dogName = "Tore";
-const tore = [dogName, "Miniature Bull Terrier", 2021-2018, "He likes sleeping in his fluffy bed", dogs];
-
-console.log(tore);
-console.log(tore.length);
-console.log(tore[2]);
-
-
-const calcAge = function(birthYear) { // Function to calculate age
-	return 2021 - birthYear;
-};
-
-const yearOfBirth = [1999, 1985, 2001, 2016, 1978]; // Array with birth years
-
-const firstAge = calcAge(yearOfBirth[0]); // You need to do each calculation seperately
-
-const secondAge = calcAge(yearOfBirth[1]); // Seclecting which array index to calculate.
-
-console.log(firstAge, secondAge);
+console.log(calcAge(20, 10));
